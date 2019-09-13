a7d4336d12397b947e39c34f558bfb268cfe4cee
masan67 <mariosanchezramirez67@gmail.com>
2019-09-13 00:24:42 -0500

    lottie

---
+++ app/src/main/java/com/marioapps/collapsingtoolbartest/LottieActivity.java
@@ -0,0 +1,17 @@
+package com.marioapps.collapsingtoolbartest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LottieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottie);
+
+
+
+    }
+}
--- app/src/main/java/com/marioapps/collapsingtoolbartest/MainActivity.java
+++ app/src/main/java/com/marioapps/collapsingtoolbartest/MainActivity.java
@@ -2,15 +2,20 @@ package com.marioapps.collapsingtoolbartest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 
 import android.webkit.WebView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     public static WebView webview;
+    private FloatingActionButton fab;
     public static String mURL = "https://antonioleiva.com";
 
     @Override
@@ -18,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LottieActivity.class);
+                startActivity(intent);
+            }
+        });
         webview = findViewById(R.id.webview);
 
         webview.getSettings().setJavaScriptEnabled(true);
